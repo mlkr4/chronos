08e1f6392bc49f8cec8010cc3aa0c640b090d0f1
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-09-18 16:30:33 +0200

    Update network-scan.py
    scaner bugfixes

--- network-scan.py
+++ network-scan.py
@@ -5,7 +5,7 @@ import chronosdb, confighelper
 
 config = confighelper.read_config()
 
-macs = (config["Scanner"]["mac"]).split(",")
+macs = (config["Scanner"]["mac"]).upper().split(",")
 print(macs)
 macQuery = 0
 for x in range(45):
@@ -13,12 +13,15 @@ for x in range(45):
     nm.scan(hosts=config["Scanner"]["subnet"], arguments='-sP')
     host_list = nm.all_hosts()
     for host in host_list:
-        if  'mac' in nm[host]['addresses']:
+        if 'mac' in nm[host]['addresses']:
             for mac in macs:
-                print(host+' : '+nm[host]['addresses']['mac'])
+                # print(mac)
+                # print(host+' : '+nm[host]['addresses']['mac'])
                 if mac == nm[host]['addresses']['mac']:
                     macQuery = 1
+                    print(mac + " : found in " + nm[host]['addresses']['mac'] + ", macQuery = " + str(macQuery))
 
+print("Final macQuery: ", macQuery)
 db = chronosdb.DBAction()
 db.InsertPresence(macQuery)
 db.Close()
