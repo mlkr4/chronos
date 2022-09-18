7df13c4e41c1f5261e2df7c7979827568e384df5
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-09-18 12:56:43 +0200

    scanner takes more mac addresses

--- network-scan.py
+++ network-scan.py
@@ -5,26 +5,19 @@ import chronosdb, confighelper
 
 config = confighelper.read_config()
 
-danMac = config.["Scanner"]["mac1"].upper()
-misMac = config.["Scanner"]["mac2"].upper()
-danQuery = 0
-misQuery = 0
+macs = (config.["Scanner"]["mac"]).split(",")
+macQuery = 0
 for x in range(45):
     nm = nmap.PortScanner()
     nm.scan(hosts=config.["Scanner"]["subnet"], arguments='-sP')
     host_list = nm.all_hosts()
     for host in host_list:
         if  'mac' in nm[host]['addresses']:
-            #print(host+' : '+nm[host]['addresses']['mac'])
-            if danMac == nm[host]['addresses']['mac']:
-                danQuery = 1
-            if misMac == nm[host]['addresses']['mac']:
-                misQuery = 1
-
-print("INSERT INTO dev_connectiontest (dan, mis) VALUES (", danQuery,", ", misQuery,")")
-
-#if DanQuery or MisaQuery:
+            for mac in macs:
+                #print(host+' : '+nm[host]['addresses']['mac'])
+                if mac == nm[host]['addresses']['mac']:
+                    macQuery = 1
 
 db = chronosdb.DBAction()
-db.InsertPresence(danQuery, misQuery)
+db.InsertPresence(macQuery)
 db.Close()
