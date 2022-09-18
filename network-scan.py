c8e404fb56cd5bfc3589031287a9ea03165445e5
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-09-18 13:59:07 +0200

    Update network-scan.py
    syntax error

--- network-scan.py
+++ network-scan.py
@@ -5,11 +5,11 @@ import chronosdb, confighelper
 
 config = confighelper.read_config()
 
-macs = (config.["Scanner"]["mac"]).split(",")
+macs = (config["Scanner"]["mac"]).split(",")
 macQuery = 0
 for x in range(45):
     nm = nmap.PortScanner()
-    nm.scan(hosts=config.["Scanner"]["subnet"], arguments='-sP')
+    nm.scan(hosts=config["Scanner"]["subnet"], arguments='-sP')
     host_list = nm.all_hosts()
     for host in host_list:
         if  'mac' in nm[host]['addresses']:
