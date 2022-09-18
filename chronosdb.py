6c039a27311581e24a6471e7d7f2f1c02b2c34f0
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-09-18 12:57:03 +0200

    DB rework

--- chronosdb.py
+++ chronosdb.py
@@ -22,11 +22,31 @@ class DBAction:
     def Close(self):
         self.mydb.close()
 
+    def CheckDatabases(self):
+        cur = self.mydb.cursor()
+        cur.execute('SHOW TABLES')
+        sql = cur.fetchall()
+        print(f"sql")
+
+    def InitDatabase(self):
+        cur = self.mydb.cursor()
+        try:
+            tableName = config["DB"]["presencetable"]
+            cur.execute("CREATE TABLE IF NOT EXISTS {tableName} ( `timestamp` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `state` TINYINT(1) NOT NULL , UNIQUE (`timestamp`)) ")
+        except mydb.Error as e:
+            print(f"Error creating table: {e}")
+        try:
+            tableName = config["DB"]["servertable"]
+            cur.execute("CREATE TABLE IF NOT EXISTS {tableName} ( `timestamp` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , `status` TINYTEXT NOT NULL , `event` TEXT NOT NULL , UNIQUE (`timestamp`))")
+        except mydb.Error as e:
+            print(f"Error creating table: {e}")
+
     def CheckPresence(self):
+        tableName = config["DB"]["presencetable"]
         data = tuple()
         result = 0
         cur = self.mydb.cursor()
-        cur.execute('SELECT dan, mis FROM dev_connectiontest ORDER BY date DESC LIMIT 4')
+        cur.execute('SELECT state FROM {tableName} ORDER BY timestamp DESC LIMIT 4')
         sql = cur.fetchall()
         print(sql)
         for row in sql:
@@ -39,18 +59,30 @@ class DBAction:
         if result == 0: return False
         else: return True
 
-    def InsertPresence(self, danPresence, misPresence):
+    def InsertPresence(self, presence):
+        tableName = config["DB"]["presencetable"]
         cur = self.mydb.cursor()
         try:
-            cur.execute("INSERT INTO dev_connectiontest (dan, mis) VALUES (?, ?)", (danPresence, misPresence))
+            cur.execute("INSERT INTO {tableName} (state) VALUES (?)", (presence))
             self.mydb.commit()
             print(cur.rowcount, "record inserted.")
         except mariadb.Error as e:
-            print(f"Error: {e}")
+            print(f"InsertPresence error: {e}")
 
 if __name__ == '__main__':
     db = DBAction()
-    #db.InsertPresence(1, 1)
-    db.CheckPresence()
+    print("[I]nitDatabase")
+    print("[C]heck presence")
+    print("Insert [P]resence")
+    action = input("Your choice? ").upper()
+    if action == "I":
+        db.InitDatabase()
+        print("done")
+    elif action == "C":
+        print("DB: presence state : ")
+        db.CheckPresence()
+    elif action == "P":
+        db.InsertPresence(1)
+        print("done")
     db.Close()
     #print("Presence DB returns {", db.CheckPresence(), "}")
