5befa8b926ef5506f947b1f410d2b8101664626e
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-09-22 07:46:48 +0200

    Update _argparser.py
    bugfix

--- _argparser.py
+++ _argparser.py
@@ -9,10 +9,10 @@ subparsers = parser.add_subparsers(help='commands')
 # check command
 
 checkParser = subparsers.add_parser("check", help='Check last entry')
-checkParser.add_argument("presence", "serverstate", action = "store" help = "DB table to check")
+checkParser.add_argument("presence", "serverstate", action = "store", help = "DB table to check")
 
 insertParser = subparsers.add_parser("insert", help = "insert value into table")
-insertParser.add_argument("presence", "serverstate", action = "store" help = "DB table to insert")
-insertParser.add_argument("true", "false", action = "store" help = "value to insert")
+insertParser.add_argument("presence", "serverstate", action = "store", help = "DB table to insert")
+insertParser.add_argument("true", "false", action = "store", help = "value to insert")
 
 print(parser.parse_args())
