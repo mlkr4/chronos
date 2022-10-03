1c82db307042eec7cae7268db39a42b05d5e92c1
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-10-03 16:12:49 +0200

    betterment debug info

--- chronosdb.py
+++ chronosdb.py
@@ -9,7 +9,7 @@ import confighelper
 
 class Databaser:
     def __init__(self):
-        logging.debug("chronosdb: Databaser init")
+        logging.debug("chronosdb: Databaser> init")
         self.config = confighelper.read_config()
         self.presenceRowSum = 0
         try:
@@ -19,107 +19,113 @@ class Databaser:
             password=self.config["DB"]["password"],
             database=self.config["DB"]["database"]
             )
-            logging.info("chronosdb: Databaser init: MariaDB connect succesfull")
+            logging.info("chronosdb: Databaser.init> MariaDB connect succesfull")
         except mariadb.Error as e:
-            logging.error("chronosdb: Databaser init: Error connecting to MariaDB Platform: {e}")
+            logging.error("chronosdb: Databaser.init> Error connecting to MariaDB Platform: {e}")
             sys.exit(1)
 
     def Close(self):
         self.mydb.close()
-        logging.info("chronosdb: Databaser Close: Succesfully closed MariaDB platform.")
+        logging.info("chronosdb: Databaser.Close> Succesfully closed MariaDB platform.")
 
     def InitDatabase(self):
+        logging.debug("chronosdb: Databaser.InitDatabase> init")
         cur = self.mydb.cursor()
         try:
             tableName = self.config["DB"]["presencetable"]
             query = "CREATE TABLE IF NOT EXISTS " + tableName + " (timestamp TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , state TINYINT(1) NOT NULL , UNIQUE (timestamp))"
-            logging.debug("chronosdb InitDatabase: built query: " + query)
+            logging.debug("chronosdb: Databaser.InitDatabase> built query: " + query)
             cur.execute(query)
             self.mydb.commit()
-            logging.info("chronosdb InitDatabase: presencetable commited.")
+            logging.info("chronosdb: Databaser.InitDatabase> presencetable commited.")
         except mariadb.Error as e:
-            logging.error("chronosdb InitDatabase: Error creating table: {e}")
+            logging.error("chronosdb: Databaser.InitDatabase> Error creating table: {e}")
         try:
             tableName = self.config["DB"]["servertable"]
             query = "CREATE TABLE IF NOT EXISTS " + tableName + " (timestamp TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP , status TINYTEXT NOT NULL , event TEXT NOT NULL , UNIQUE (timestamp))"
-            logging.debug("chronosdb InitDatabase: built query: " + query)
+            logging.debug("chronosdb: Databaser.InitDatabase> built query: " + query)
             cur.execute(query)
             self.mydb.commit()
-            logging.info("chronosdb InitDatabase: servertable commited.")
+            logging.info("chronosdb: Databaser.InitDatabase> servertable commited.")
         except mariadb.Error as e:
-            logging.error("chronosdb InitDatabase: Error creating table: {e}")
+            logging.error("chronosdb Databaser.InitDatabase> Error creating table: {e}")
 
     def CheckPresence(self):
+        logging.debug("chronosdb: Databaser.CheckPresence> init")
         self.presenceRowSum = 0
         tableName = self.config["DB"]["presencetable"]
         result = False
         cur = self.mydb.cursor()
         query = "SELECT state FROM " + tableName + " ORDER BY timestamp DESC LIMIT 4"
-        logging.debug("chronosdb CheckPresence: built query: " + query)
+        logging.debug("chronosdb: Databaser.CheckPresence> built query: " + query)
         cur.execute(query)
         response = cur.fetchall()
-        logging.debug("chronosdb CheckPresence: query response caught: {a}".format(a = response))
+        logging.debug("chronosdb: Databaser.CheckPresence> query response caught: {a}".format(a = response))
         for row in response:
             self.presenceRowSum += row[0]
-            logging.debug("chronosdb CheckPresence: self.presenceRowSum == {a}".format(a = self.presenceRowSum))
+            logging.debug("chronosdb: Databaser.CheckPresence> self.presenceRowSum == {a}".format(a = self.presenceRowSum))
             if row[0] == 1: 
-                logging.debug("chronosdb CheckPresence: found active presence, setting result = true")
+                logging.debug("chronosdb: Databaser.CheckPresence> found active presence, setting result = true")
                 result = True
-        logging.info("chronosdb CheckPresence: returning {a}".format(a = result))
+        logging.info("chronosdb: Databaser.CheckPresence> returning {a}".format(a = result))
         return result
 
     def InsertPresence(self, state):
+        logging.debug("chronosdb: Databaser.InsertPresence> init")
         tableName = self.config["DB"]["presencetable"]
-        logging.debug("chronosdb InsertPresence: calling self.CheckPresence, self.presenceRowSum == {a}".format(a = self.presenceRowSum))
+        logging.debug("chronosdb Databaser.InsertPresence> calling self.CheckPresence, self.presenceRowSum == {a}".format(a = self.presenceRowSum))
         presenceState = self.CheckPresence()
-        logging.debug("chronosdb InsertPresence: self.presenceRowSum == {a}".format(a = self.presenceRowSum))
+        logging.debug("chronosdb: Databaser.InsertPresence> self.presenceRowSum == {a}".format(a = self.presenceRowSum))
         if (self.presenceRowSum in range(1,4)) or (state is not presenceState):
-            logging.debug("chronosdb InsertPresence: (self.presenceRowSum [{a}] in range(1,4)) or (state [{b}] =! presenceState [{c}]) evaluated as TRUE".format(a = self.presenceRowSum, b = state, c = presenceState))
+            logging.debug("chronosdb: Databaser.InsertPresence> (self.presenceRowSum [{a}] in range(1,4)) or (state [{b}] =! presenceState [{c}]) evaluated as TRUE".format(a = self.presenceRowSum, b = state, c = presenceState))
             cur = self.mydb.cursor()
             try:
-                logging.debug("chronosdb InsertPresence: built query INSERT INTO {a} (state) VALUE ({b})".format(a = tableName, b = state))
-                cur.execute("INSERT INTO {a} (state) VALUE (?)".format(a = tableName), (state, ))
+                query = "INSERT INTO {a} (state) VALUE (?)".format(a = tableName)
+                logging.debug("chronosdb: Databaser.InsertPresence> built query: {a}".format(a = query))
+                cur.execute(query, (state, ))
                 self.mydb.commit()
-                logging.info("chronosdb InsertPresence: {a} record inserted.".format(a = cur.rowcount))
+                logging.info("chronosdb: Databaser.InsertPresence> {a} record inserted.".format(a = cur.rowcount))
             except mariadb.Error as e:
-                logging.error("chronosdb InsertPresence: error: {e}".format())
+                logging.error("chronosdb: Databaser.InsertPresence> error: {e}".format())
         else:
-            logging.debug("chronosdb InsertPresence: (self.presenceRowSum [{a}] in range(1,4)) or (state [{b}] =! presenceState [{c}]) evaluated as FALSE".format(a = self.presenceRowSum, b = state, c = presenceState))
-            logging.debug("chronosdb InsertPresence: nothing to do.")
+            logging.debug("chronosdb: Databaser.InsertPresence> (self.presenceRowSum [{a}] in range(1,4)) or (state [{b}] =! presenceState [{c}]) evaluated as FALSE".format(a = self.presenceRowSum, b = state, c = presenceState))
+            logging.debug("chronosdb: Databaser.InsertPresence> nothing to do.")
 
     def CheckSrvState(self):
         tableName = self.config["DB"]["servertable"]
         result = False
         cur = self.mydb.cursor()
-        logging.debug("chronosdb CheckState: built query: SELECT status FROM {a} ORDER BY timestamp DESC LIMIT 1".format(a = tableName))
-        cur.execute("SELECT status FROM {a} ORDER BY timestamp DESC LIMIT 1".format(a = tableName))
+        query = "SELECT status FROM {a} ORDER BY timestamp DESC LIMIT 1".format(a = tableName)
+        logging.debug("chronosdb: Databaser.CheckState> built query: {a}".format(a = query))
+        cur.execute(query)
         response = cur.fetchall()
-        logging.debug("chronosdb CheckState: query response caught: {a}".format(a = response))
+        logging.debug("chronosdb: CheckState> query response caught: {a}".format(a = response))
         for row in response:
             if row[0] == 1:
-                logging.debug("chronosdb CheckState: found active state, setting result = true")
+                logging.debug("chronosdb: Databaser.CheckState> found active state, setting result = true")
                 result = True
             elif row[0] == 0:
-                logging.debug("chronosdb CheckState: found active state, setting result = false")
+                logging.debug("chronosdb: Databaser.CheckState> found active state, setting result = false")
                 result = False
-        logging.info("chronosdb CheckState: returning {a}".format(a = result))
+        logging.info("chronosdb: Databaser.CheckState> returning {a}".format(a = result))
         return result
 
     def InsertSrvState(self, status, event):
-        logging.debug("chronosdb InsertSrvState: input parameters status = {a}, event = {b}".format(a = status, b = event))
+        logging.debug("chronosdb: Databaser.InsertSrvState> input parameters status = {a}, event = {b}".format(a = status, b = event))
         tableName = self.config["DB"]["servertable"]
         cur = self.mydb.cursor()
         try:
-            logging.debug("chronosdb InsertSrvState: built query: INSERT INTO {a} (status, event) VALUES ({b}, {c})".format(a = tableName, b = status, c = event))
-            cur.execute("INSERT INTO {a} (status, event) VALUES (?, ?)".format(a = tableName), (status, event))
+            query = "INSERT INTO {a} (status, event) VALUES (?, ?)".format(a = tableName)
+            logging.debug("chronosdb: Databaser.InsertSrvState> built query: {a}".format(a = query))
+            cur.execute(query, (status, event))
             self.mydb.commit()
-            logging.info("chronosdb InsertSrvState: {a} record inserted.".format(a = cur.rowcount))
+            logging.info("chronosdb: Databaser.InsertSrvState> {a} record inserted.".format(a = cur.rowcount))
         except mariadb.Error as e:
-            logging.error("chronosdb InsertSrvState: error: {e}")
+            logging.error("chronosdb Databaser.InsertSrvState> error: {e}")
 
 
 if __name__ == '__main__':
-    logging.info("chronosdb started")
+    logging.info("chronosdb> main")
     db = Databaser()
     print("[I]nitDatabase")
     print("[C]heck presence")
@@ -142,4 +148,4 @@ if __name__ == '__main__':
     elif action == "N":
         db.InsertSrvState(False, "chronosdb dry run")
     db.Close()
-    logging.info("chronosdb finished")
+    logging.info("chronosdb> fin.")
--- chronossrv.py
+++ chronossrv.py
@@ -11,22 +11,24 @@ from typing import List
 
 class Server():
     def __init__(self, IP, mac, acc, cert):
-        logging.debug("chronossrv: Server init")
+        logging.debug("chronossrv: Server> init")
         self.serverIP = IP
         self.serverMac = mac
         self.serverAcc = acc
         self.serverCert = cert
 
     def CreateMagicPacket(self, macaddress: str) -> bytes:
+        logging.debug("chronossrv: Server.CreateMagicPacket> init")
         if len(macaddress) == 17:
             sep = macaddress[2]
             macaddress = macaddress.replace(sep, "")
         elif len(macaddress) != 12:
             raise ValueError("Incorrect MAC address format")
+        logging.debug("chronossrv: Server.CreateMagicPacket> Fin.")
         return bytes.fromhex("F" * 12 + macaddress * 16)
 
     def Wake(self):
-        logging.debug('chronossrv: Server.Wake(): initializing.')
+        logging.debug('chronossrv: Server.Wake> init')
         ip_address = "255.255.255.255"
         port = 9
         interface = None
@@ -37,40 +39,40 @@ class Server():
             sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
             sock.connect((ip_address, port))
             sock.send(packet)
-            logging.info('chronossrv: Server.Wake(): Magic packet sent.')
+            logging.info('chronossrv: Server.Wake> Magic packet sent.')
             return True
 
     def Poweroff(self):
-            logging.debug('chronossrv: Server.Poweroff(): initializing.')
+            logging.debug('chronossrv: Server.Poweroff> init')
             if subprocess.Popen(f"ssh -i{self.serverCert} {self.serverAcc}@{self.serverIP} sudo poweroff", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate():
-                logging.debug("chronossrv: Server.Poweroff(): sent: ssh -i{rsaCertificate} {serverAcc}@{serverIP} sudo poweroff")
-                logging.info('chronossrv: Server.Poweroff(): Poweroff sent.')
-                logging.debug("chronossrv: Server.Poweroff(): Returning True")
+                logging.debug("chronossrv: Server.Poweroff> sent: ssh -i{rsaCertificate} {serverAcc}@{serverIP} sudo poweroff")
+                logging.info('chronossrv: Server.Poweroff> Poweroff sent.')
+                logging.debug("chronossrv: Server.Poweroff> Returning True")
                 return True
             else:
-                logging.error('chronossrv: Server.Poweroff(): Returning False')
+                logging.error('chronossrv: Server.Poweroff> Cannot send SSH command, returning False')
                 return False
 
     def Ping(self):
-        logging.debug("chronossrv: Server.Ping(): initializing")
+        logging.debug("chronossrv: Server.Ping> init")
         hostUp = True if os.system("ping -c 1 " + self.serverIP) == 0 else False
-        logging.debug("chronossrv: Ping: sent ping -c 1 {serverIP}, evaluated as {hostUP}.")
+        logging.debug("chronossrv: Server.Ping> sent ping -c 1 {serverIP}, evaluated as {hostUP}.")
         if hostUp:
-            logging.info("chronossrv: Server.Ping(): host is up, returning true")
+            logging.info("chronossrv: Server.Ping> host is up, returning True")
             return True
         else:
-            logging.info("chronossrv: Server.Ping(): host is down, returning false")
+            logging.info("chronossrv: Server.Ping> host is down, returning False")
             return False
 
 def main(argv: List[str] = None) -> None:
-    logging.debug('chronossrv: main: initializing.')
+    logging.debug('chronossrv: main> init.')
     parser = argparse.ArgumentParser(description = "Control configured computer - WoL, shutdown, ping, verify.", formatter_class = argparse.ArgumentDefaultsHelpFormatter,)
     parser.add_argument(dest = "controlArg", choices=["ping", "shutdown", "verify", "wake"], help = "wake | shutdown | ping | verify")
     args = parser.parse_args()
-    logging.debug('chronossrv: main: arguments parsed: {a}'.format(a = args))
+    logging.debug('chronossrv: main> arguments parsed: {a}'.format(a = args))
 
     if args.controlArg:
-        logging.debug("chronossrv: main: control argument passed.")
+        logging.debug("chronossrv: main> control argument passed.")
 
         config = confighelper.read_config()
         serverIP = config["Server"]["IP"]
@@ -78,10 +80,10 @@ def main(argv: List[str] = None) -> None:
         serverAcc = config["Server"]["username"]
         rsaCertificate = config["Server"]["rsaCertificate"]
     
-        logging.debug("chronossrv: main: initializing Computer object.")
+        logging.debug("chronossrv: main> calling Server().")
         computer = Server(serverIP, serverMac, serverAcc, rsaCertificate)
 
-        logging.debug("chronossrv: main: initializing DB object.")
+        logging.debug("chronossrv: main> calling chronosdb.Databaser()")
         db = chronosdb.Databaser()
 
         if args.controlArg == "wake":
@@ -114,9 +116,11 @@ def main(argv: List[str] = None) -> None:
                     print("Server is unresponsive, DB state inconsistent, fixed")
                 else:
                     print("Server is unresponsive, DB state consistent")
+        logging.debug("chronossrv: main> Closing Databaser.")
         db.Close()
     else:
         print("Nothing to do.")
 
 if __name__ == '__main__':
     main()
+    logging.debug("chronossrv: main> Fin.")
