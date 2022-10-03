05a6b0f4eba2f282fa1c332753bb2dbf50a65486
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2022-10-03 16:12:49 +0200

    Debug info improvement

--- chronos.py
+++ chronos.py
@@ -6,48 +6,65 @@ import confighelper, chronosdb, chronossrv, chronostimer, chronosscan
 logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(filename='event.log', filemode='w', format='%(asctime)s: %(name)s - %(levelname)s - %(message)s')
 
-config = confighelper.read_config()
-
 def VerifyPresence(db):
+    logging.debug("Chronos: VerifyPresence> calling Scanner.Scan")
     scanner = chronosscan.Scanner()
     result = scanner.Scan()
+    logging.debug("Chronos: VerifyPresence> scan returned {a}".format(a = result))
+    logging.debug("Chronos: VerifyPresence> calling Databaser.InsertPresence({a})".format(a = result))
     db.InsertPresence(result)
+    logging.debug("Chronos: VerifyPresence> calling Databaser.CheckPresence()")
     result = db.CheckPresence()
+    logging.debug("Chronos: VerifyPresence> returning Databaser.CheckPresence() result = {a}".format(a = result))
     return result
 
 if __name__ == '__main__':
+
+    logging.debug("Chronos> Reading config from confighelper")
+    config = confighelper.read_config()
 
     serverIP = config["Server"]["IP"]
     serverMac = config["Server"]["Mac"]
     serverAcc = config["Server"]["username"]
     rsaCertificate = config["Server"]["rsaCertificate"]
 
+    logging.debug("Chronos> Initializing chronossrv.Server")
     server = chronossrv.Server(serverIP, serverMac, serverAcc, rsaCertificate)
+    logging.debug("Chronos> Initializing chronosdb.Databaser")
     db = chronosdb.Databaser()
+    logging.debug("Chronos> Initializing chronostimer.Timer")
     timer = chronostimer.Timer()
 
+    logging.debug("Chronos> calling timer.PresenceCheckBeforePoweron()")
     if timer.PresenceCheckBeforePoweron():
+        logging.debug("Chronos> timer.PresenceCheckBeforePoweron() returned {a}, calling VerifyPresnece()".format(a = True))
         VerifyPresence(db)
 
+    logging.debug("Chronos> Calling server.Ping()")
     if server.Ping():
+        logging.debug("Chronos> Server.Ping() returned True")
+        logging.debug("Chronos> Calling timer.SurpressPoweron() elif VerifyPresence()")
         if timer.SurpressPoweron():
-            logging.info("Calling shutdown sequence based on SurpressPoweron")
+            logging.info("Chronos> Calling shutdown sequence based on SurpressPoweron")
             server.Poweroff()
             db.InsertSrvState(False, "Chronos: SHUTDOWN CALL by SURPRESS")
         elif not VerifyPresence(db):
             server.Poweroff()
             db.InsertSrvState(False, "Chronos: SHUTDOWN CALL by PRESENCE")
-            logging.info("Calling shutdown sequence based on PresenceState")
+            logging.info("Chronos> Called shutdown sequence based on VerifyPresence")
         else:
-            logging.debug("No PWR change based on PowerState, PresenceState, !SurpressPoweron")
+            logging.debug("Chronos> No PWR change based on Ping, !SurpressPoweron, VerifyPresence()")
     else:
+        logging.debug("Chronos> Server.Ping() returned False")
         if not timer.SurpressPoweron():
             if VerifyPresence(db):
-                logging.info("Calling startup sequence based on PresenceState and !SurpressPoweron")
+                logging.info("Chronos> Calling startup sequence based on !Ping, !SurpressPoweron and VerifyPresence")
                 server.Wake()
                 db.InsertSrvState(True, "Chronos: WAKE CALL by PRESENCE")
             else:
-                logging.debug("No PWR change based on !PowerState, !PresenceState, !SurpressPoweron")
+                logging.debug("Chronos> No PWR change based on !Ping, !SurpressPoweron and !VerifyPresence")
         else:
-            logging.debug("No PWR change based on !SurpressPoweron")
+            logging.debug("Chronos> No PWR change based on !SurpressPoweron")
+    logging.debug("Chronos> calling Databaser.Close()")
     db.Close()
+    logging.debug("Chronos> Fin.")
--- chronostimer.py
+++ chronostimer.py
@@ -10,83 +10,99 @@ from datetime import datetime
 
 class Timer():
     def __init__(self):
-        logging.debug("Timer innit commenced")
+        logging.debug("chronostimer: Timer> init")
         config = confighelper.read_config()
         self.now = datetime.now()
         self.quiesceStartTime = int(config["Timer"]["quiesceStartTime"])
-        logging.debug("Timer: init: quiesceStartTime read from config as {a}".format(a = self.quiesceStartTime))
+        logging.debug("chronostimer: Timer.init> quiesceStartTime read from config as {a}".format(a = self.quiesceStartTime))
         self.quiesceEndTime = int(config["Timer"]["quiesceEndTime"])
-        logging.debug("Timer: init: quiesceEndTime read from config as {a}".format(a = self.quiesceEndTime))
+        logging.debug("chronostimer: Timer.init> quiesceEndTime read from config as {a}".format(a = self.quiesceEndTime))
         self.noPoweroffDay = config["Timer"]["quiesceDays"].split(",")
         self.noPoweroffDay = [int(s) for s in self.noPoweroffDay]
-        logging.debug("Timer: init: noPoweroffDay read from config as {a}".format(a = self.noPoweroffDay))
+        logging.debug("chronostimer: Timer.init> noPoweroffDay read from config as {a}".format(a = self.noPoweroffDay))
         self.currentTime = 100*self.now.hour + self.now.minute
         self.currentDay = self.now.weekday()                    # [0-6] - TBD: CurrentDayEval() - evaluate against holidays
+        logging.debug("chronostimer: Timer.init> fin.")
 
     def Report(self):
+        logging.debug("chronostimer: Timer.Report> init")
         print("Time now: {a} on day {b}".format(a = 100*self.now.hour + self.now.minute, b = self.now.weekday()))
         print("quiesceStartTime - quiesceEndTime set to: {a} - {b}".format(a = self.quiesceStartTime, b = self.quiesceEndTime))
         print("SurpressPoweron returns: {a}.".format(a = self.SurpressPoweron()))
         print("PresenceCheckBeforePoweron returns: {a}".format(a = self.PresenceCheckBeforePoweron()))
+        logging.debug("chronostimer: Timer.Report> fin.")
 
     # TBD Holidays - build into init prolly?
     def SurpressPoweronByDate(self):
+        logging.debug("chronostimer: Timer.SurpressPoweronByDate> init")
+        logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day ({a}) evaluated as outside of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
         if not self.currentDay in self.noPoweroffDay:
-            logging.debug("SurpressPoweronByDate: Current day ({a}) evaluated as outside of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
+            logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day ({a}) evaluated as outside of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
             if ((self.currentDay + 1) in self.noPoweroffDay) or ((self.currentDay - 6) in self.noPoweroffDay):
-                logging.debug("SurpressPoweronByDate: Current day ({a}) evaluated as day before of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
+                logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day ({a}) evaluated as day before of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
                 if (self.currentTime < self.quiesceEndTime):
-                    logging.debug("SurpressPoweronByDate: Quiesce time evaluated as true")            
+                    logging.debug("chronostimer: Timer.SurpressPoweronByDate> Quiesce time evaluated as true")            
                     return True
                 else:
-                    logging.debug("SurpressPoweronByDate: Quiesce time evaluated as false")
+                    logging.debug("chronostimer: Timer.SurpressPoweronByDate> Quiesce time evaluated as false")
                     return False
             else:
-                logging.debug("SurpressPoweronByDate: Current day evaluated as not before noPoweroffDay")
+                logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day evaluated as not before noPoweroffDay")
                 if ((self.currentTime < self.quiesceEndTime) or (self.currentTime >= self.quiesceStartTime)):
-                    logging.debug("SurpressPoweronByDate: Quiesce time evaluated as true")            
+                    logging.debug("chronostimer: Timer.SurpressPoweronByDate> Quiesce time evaluated as true")            
                     return True
                 else: 
-                    logging.debug("SurpressPoweronByDate: Quiesce time evaluated as false")
+                    logging.debug("chronostimer: Timer.SurpressPoweronByDate> Quiesce time evaluated as false")
                     return False
         else:
-            logging.debug("SurpressPoweronByDate: Current day ({a}) evaluated as inside of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
+            logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day ({a}) evaluated as inside of noPoweroffDay ({b})".format(a = self.currentDay, b = self.noPoweroffDay))
             if ((self.currentDay + 1) in self.noPoweroffDay) or (self.currentDay - 6) in self.noPoweroffDay:
-                logging.debug("SurpressPoweronByDate: Current day evaluated as inside of noPoweroffDay, quiesce time evaluated as false")
+                logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day evaluated as inside of noPoweroffDay, quiesce time evaluated as false")
                 return False
             else:
-                logging.debug("SurpressPoweronByDate: Current day evaluated as in the end of noPoweroffDay")
+                logging.debug("chronostimer: Timer.SurpressPoweronByDate> Current day evaluated as in the end of noPoweroffDay")
                 if (self.currentTime >= self.quiesceStartTime):
-                    logging.debug("SurpressPoweronByDate: Quiesce time evaluated as true")
+                    logging.debug("chronostimer: Timer.SurpressPoweronByDate> Quiesce time evaluated as true")
                     return True
                 else:
-                    logging.debug("SurpressPoweronByDate: Quiesce time evaluated as false")
+                    logging.debug("chronostimer: Timer.SurpressPoweronByDate> Quiesce time evaluated as false")
                     return False
+        logging.debug("chronostimer: Timer.SurpressPoweronByDate> fin.")
 
     def SurpressPoweron(self):
+        logging.debug("chronostimer: Timer.SurpressPoweron> init")
         if self.SurpressPoweroffByOverride():
-            logging.debug("SurpressPoweron: SurpressPoweroffByOverride evaluated as true, returning false")
+            logging.debug("chronostimer: Timer.SurpressPoweron> SurpressPoweroffByOverride evaluated as true, returning false")
             return False
         elif self.SurpressPoweronByDate():
-            logging.debug("SurpressPoweron: SurpressPoweronByDate evaluated as true, returning true")
+            logging.debug("chronostimer: Timer.SurpressPoweron> SurpressPoweronByDate evaluated as true, returning true")
             return True
         else:
-            logging.debug("SurpressPoweron: defaulting to false")
+            logging.debug("chronostimer: Timer.SurpressPoweron> defaulting to false")
             return False
+        logging.debug("chronostimer: Timer.SurpressPoweron> fin.")
     
     def SurpressPoweroffByOverride(self):
         # cekni hardlocky proti DB
-        logging.debug("SurpressPoweroffByOverride: defaulting to false")
+        logging.debug("chronostimer: Timer.SurpressPoweroffByOverride> defaulting to false")
         return False
 
     def PresenceCheckBeforePoweron(self):
+        logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> init")
+        logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> checking: SurpressPoweronByDate")
         if self.SurpressPoweronByDate():
+            logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> SurpressPoweronByDate returned True")
+            logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> checking: quiesceEndTime({a}) - 100 <= currentTime({b})")
             if (self.quiesceEndTime - 100) <= self.currentTime:
+                logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> returning True")
                 return True
             else:
+                logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> returning False")
                 return False
         else:
+            logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> SurpressPoweronByDate returned False, returning False")
             return False
+        logging.debug("chronostimer: Timer.PresenceCheckBeforePoweron> fin.")
 
 if __name__ == '__main__':
 
