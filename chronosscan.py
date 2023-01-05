60cbb84dba876b30407bfaac569d53db6a1314fb
mlkr4 <58075174+mlkr4@users.noreply.github.com>
2023-01-05 10:15:41 +0100

    Update chronosscan.py
    scanning for IP

--- chronosscan.py
+++ chronosscan.py
@@ -13,6 +13,7 @@ class Scanner():
         logging.debug("chronosscan: Scanner> init")
         self.config = confighelper.read_config()
         self.macs = (self.config["Scanner"]["mac"]).upper().split(",")
+        self.ips = (self.config["Scanner"]["ip"]).upper().split(",")
         logging.debug("chronosscan: Scanner.init> Querried macs: {a}".format(a = self.macs))
 
     def is_mac_addr_present(self):
@@ -35,7 +36,31 @@ class Scanner():
         logging.info("chronosscan: Scanner.is_mac_addr_present> finished, returning {a}.".format(a = result))
         return result
 
+    def is_ip_addr_present(self):
+        logging.debug("chronosscan: Scanner.is_ip_addr_present> started")
+        result = False
+        for ip in self.ips:
+            logging.debug("chronosscan: Scanner.is_ip_addr_present> scanning for {ip}.")
+            if is_ip_addr_up(ip):
+                result = True
+            if result: break
+        logging.debug("chronosscan: Scanner.is_ip_addr_present> returning {result}.")
+        return result
+
+
+    def is_ip_addr_up(self, hostIP):
+        logging.debug("chronosscan: Scanner.is_ip_addr_up> init")
+        hostUp = True if os.system("ping -c 1 " + hostIP) == 0 else False
+        logging.debug("chronosscan: Scanner.is_ip_addr_up> sent ping -c 1 {serverIP}, evaluated as {hostUP}.")
+        if hostUp:
+            logging.info("chronossrv: Scanner.is_ip_addr_up> host is up, returning True")
+            return True
+        else:
+            logging.info("chronossrv: Scanner.is_ip_addr_up> host is down, returning False")
+            return False
+
 if __name__ == '__main__':
     result = Scanner()
     print("Running scanner, this may take a while. Works only with sudo privileges btw.")
-    print("Found presence: {a}".format(a = result.is_mac_addr_present()))
+    print("Found IP presence: {a}".format(a = result.is_ip_addr_present()))
+    print("Found mac presence: {a}".format(a = result.is_mac_addr_present()))
